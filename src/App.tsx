373a855581424e6c58e31dc5baa2f7b2a8c3937d
Minhajulislam0 <mdmishu98730@gmail.com>
2024-02-25 00:53:43 +0600

    Routing setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,27 @@
+import { Routes, Route } from "react-router-dom";
+import SignupForm from "./_auth/forms/SignupForm";
+import SigninForm from "./_auth/forms/SigninForm";
+import { Home } from "./_root/pages";
+import AuthLayout from "./_auth/forms/AuthLayout";
+import RootLayout from "./_root/RootLayout";
 import "./globals.css";
+
 const App = () => {
-  return <h1 className="text-3xl font-bold underline">Hello picista!</h1>;
+  return (
+    <main className="flex h-screen">
+      <Routes>
+        {/* public Routes */}
+        <Route element={<AuthLayout />}>
+          <Route path="/sign-in" element={<SigninForm />} />
+          <Route path="/sign-up" element={<SignupForm />} />
+        </Route>
+        {/* private Routes */}
+        <Route element={<RootLayout />}>
+          <Route index element={<Home />} />
+        </Route>
+      </Routes>
+    </main>
+  );
 };
 
 export default App;
