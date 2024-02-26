fcd4fe86a4cc90e40ad68a7711bccc271b35e057
Minhajulislam0 <mdmishu98730@gmail.com>
2024-02-26 16:22:41 +0600

    Shadcn Implimented

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { Routes, Route } from "react-router-dom";
 import SignupForm from "./_auth/forms/SignupForm";
 import SigninForm from "./_auth/forms/SigninForm";
 import { Home } from "./_root/pages";
-import AuthLayout from "./_auth/forms/AuthLayout";
+import AuthLayout from "./_auth/AuthLayout";
 import RootLayout from "./_root/RootLayout";
 import "./globals.css";
 
